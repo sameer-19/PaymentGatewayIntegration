db501f18e04cb0e2d2681e479898130e53d894cb
sameer-19 <63365261+sameer-19@users.noreply.github.com>
2022-02-08 03:00:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("btn-1").onclick = function () {
+    window.location.href = "donate.html";
+};
+document.getElementById("btn-2").onclick = function () {
+    window.location.href = "donate.html";
+};
+document.getElementById("btn-3").onclick = function () {
+    window.location.href = "donate.html";
+};
